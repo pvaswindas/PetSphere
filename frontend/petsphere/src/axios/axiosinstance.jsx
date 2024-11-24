5cc9787949922b2466d5d1d32010027c7254d9bd
pvaswindas <aswinpv1801@gmail.com>
2024-11-24 14:10:21 +0530

    fix: directory modified

--- frontend/petsphere/src/axios/axiosinstance.jsx
+++ frontend/petsphere/src/axios/axiosinstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: process.env.API_BASE_URL
 })
 
 axiosInstance.interceptors.request.use(
