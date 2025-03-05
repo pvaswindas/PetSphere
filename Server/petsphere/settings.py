8386a293d7ce36156b4b069505d77afc61583e85
pvaswindas <aswinpv1801@gmail.com>
2025-03-05 16:40:13 +0530

    feat: implement S3 bucket

--- Server/petsphere/settings.py
+++ Server/petsphere/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'rest_framework.authtoken',
+    'storages',
     'accounts',
     'user_profile',
     'sellers',
@@ -237,3 +238,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+
+AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_SIGNATURE_NAME = env.str("AWS_S3_SIGNATURE_NAME")
+AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400",
+}
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
--- Server/user_profile/models.py
+++ Server/user_profile/models.py
@@ -1,22 +1,27 @@
 from django.db import models
 from accounts.models import PetSphereUser
+from django.core.files.storage import default_storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(PetSphereUser, on_delete=models.CASCADE,
-                                related_name='profile')
+    user = models.OneToOneField(
+        PetSphereUser, on_delete=models.CASCADE, related_name="profile"
+    )
     bio = models.CharField(max_length=255, null=True, blank=True)
     cover_image = models.ImageField(
         upload_to="cover_pics/",
         null=True,
         blank=True,
-        default='default_images/profile_cover.svg'
+        default="default_images/profile_cover.svg",
+        storage=S3Boto3Storage(),
     )
     profile_picture = models.ImageField(
         upload_to="profile_pics/",
         null=True,
         blank=True,
-        default='default_images/user_avatar.svg'
+        default="default_images/user_avatar.svg",
+        storage=S3Boto3Storage(),
     )
     push_notification = models.BooleanField(default=True)
     pawstory_count = models.PositiveBigIntegerField(default=0)
@@ -29,3 +34,48 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def save(self, *args, **kwargs):
+        try:
+            old_instance = Profile.objects.get(pk=self.pk)
+
+            # Delete old profile picture from S3
+            if (
+                old_instance.profile_picture
+                and old_instance.profile_picture.name != self.profile_picture.name
+                and old_instance.profile_picture.name != "default_images/user_avatar.svg"
+            ):
+                old_instance.profile_picture.delete(save=False)
+
+            # Delete old cover image from S3
+            if (
+                old_instance.cover_image
+                and old_instance.cover_image.name != self.cover_image.name
+                and old_instance.cover_image.name != "default_images/profile_cover.svg"
+            ):
+                old_instance.cover_image.delete(save=False)
+
+        except Profile.DoesNotExist:
+            pass
+
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        # Delete profile_picture from S3 if it's not a default image
+        if (
+            self.profile_picture
+            and self.profile_picture.name != "default_images/user_avatar.svg"
+        ):
+            print(f"Deleting profile picture: {self.profile_picture.name}")
+            self.profile_picture.delete(save=False)
+
+        # Delete cover_image from S3 if it's not a default image
+        if (
+            self.cover_image
+            and self.cover_image.name != "default_images/profile_cover.svg"
+        ):
+            print(f"Deleting cover image: {self.cover_image.name}")
+            self.cover_image.delete(save=False)
+
+        super().delete(*args, **kwargs)
+
