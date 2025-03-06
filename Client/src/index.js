b7a8d7c88b9c89d6cdf23d87c01c3052f16e2172
pvaswindas <aswinpv1801@gmail.com>
2025-03-06 19:05:22 +0530

    fix: future behavior issue solved in browser router

--- Client/src/index.js
+++ Client/src/index.js
@@ -12,9 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={<LoadingPage />} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <App />
       </BrowserRouter>
     </PersistGate>
   </Provider>
 );
+
