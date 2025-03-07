49ab889a0bbbe823b6cd0d3e44d26b238f6ed41f
pvaswindas <aswinpv1801@gmail.com>
2025-03-07 20:32:00 +0530

    fix: Auth pages reloading issue

--- Client/src/routes/RestrictedRoute.jsx
+++ Client/src/routes/RestrictedRoute.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import axiosInstance from '../axios/axiosinstance';
 import LoadingPage from '../pages/LoadingPage';
 
 function RestrictedRoute({ children }) {
     const [isAuthorized, setIsAuthorized] = useState(null);
-    const navigate = useNavigate();
-
+    const [isLoading, setIsLoading] = useState(true);
+    
     const refreshAccessToken = useCallback(async () => {
         const refreshToken = localStorage.getItem('REFRESH_TOKEN');
         if (!refreshToken) return false;
-
+        
         try {
             const res = await axiosInstance.post('accounts/token/refresh/', { refresh: refreshToken });
             if (res.status === 200) {
@@ -19,15 +19,15 @@ function RestrictedRoute({ children }) {
                 return true;
             }
         } catch (error) {
-            return
+            return false;
         }
         return false;
     }, []);
-
+    
     const validateAccessToken = useCallback(async () => {
         const token = localStorage.getItem('ACCESS_TOKEN');
         if (!token) return false;
-
+        
         try {
             const decoded = jwtDecode(token);
             const now = Math.floor(Date.now() / 1000);
@@ -36,24 +36,23 @@ function RestrictedRoute({ children }) {
             }
             return true;
         } catch (error) {
-            return false
+            return false;
         }
     }, [refreshAccessToken]);
-
+    
     useEffect(() => {
         (async () => {
             const isValid = await validateAccessToken();
             setIsAuthorized(isValid);
+            setIsLoading(false);
         })();
     }, [validateAccessToken]);
-
-    useEffect(() => {
-        if (isAuthorized) navigate('/feed');
-    }, [isAuthorized, navigate]);
-
-    if (isAuthorized === null) return <LoadingPage />
-
-    return !isAuthorized ? children : null;
+    
+    if (isLoading) return <LoadingPage />;
+    
+    if (isAuthorized) return <Navigate to="/feed" replace />;
+    
+    return children;
 }
 
 export default RestrictedRoute;
