85f3bd24b7a9399213ed449013fbe29026aa4065
pvaswindas <aswinpv1801@gmail.com>
2025-03-12 16:01:13 +0530

    fix: CORS issue

--- Server/petsphere/settings.py
+++ Server/petsphere/settings.py
@@ -144,25 +144,6 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
 
 # AUTHENTICATION
 # ------------------------------------------------------------------------------
